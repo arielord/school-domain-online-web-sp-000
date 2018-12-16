39a7e367fed647c79f6d7643612f80178c9db0ff
Ariel <arielord@buffalo.edu>
2018-12-16 19:37:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+  #attr_reader :name
+  
+  ROSTER = {}
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+    ROSTER
+  end
+  
+  def add_student(name, grade)
+    #ROSTER.key?(grade) ? ROSTER[grade] << name : ROSTER[grade] = [name]
+    if ROSTER.key?(grade)
+      ROSTER[grade] << name
+    else
+      ROSTER[grade] = [name]
+    end
+    ROSTER
+  end
+  
+  def grade(grade)
+    ROSTER.key?(grade) ? ROSTER[grade] : nil
+  end
+  
+  def sort
+    ROSTER.collect {|key, value| value.sort}
+    ROSTER
+  end
+  
+end
